9c17a0d37bad9da229442099dbd7603b78dabfca
Emily Port <emilylport@gmail.com>
2022-11-06 16:55:21 -0700

    outings appear on contestant show page

--- spec/features/contestants/show_spec.rb
+++ spec/features/contestants/show_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'contestants show page' do
     @amy = Bachelorette.create!(name: "Amy", season_number: 8, description: "fun times")
     @suzie = Bachelorette.create!(name: "Suzie", season_number: 10, description: "great experience")
     @mark = @mary.contestants.create!(name: "Mark", age: 30, hometown: "Denver")
-    @fun = Outing.create!(bachelorette_id: @mary, contestant_id: @mark)
+    @fun = Outing.create!(bachelorette_id: @mary.id, contestant_id: @mark.id, name: "Bowling", location: "Denver", date: "November 6th 2022")
     @steve = @mary.contestants.create!(name: "Steve", age: 29, hometown: "Aurora")
 
   end
@@ -16,5 +16,7 @@ RSpec.describe 'contestants show page' do
     expect(page).to have_content("Mark")
     expect(page).to have_content(12)
     expect(page).to have_content("coolest season")
+    # require 'pry'; binding.pry
+    expect(page).to have_content("Bowling")
   end 
 end
